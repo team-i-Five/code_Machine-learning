51428b9952d15ee22dcea59ad6f1a120ee47df2f
qkrtnqls1216 <qkrtnqls991216@gmail.com>
2023-11-28 14:26:07 +0900

    FAST_API_FINAL

--- Fast_API/app_nmf.py
+++ Fast_API/app_nmf.py
@@ -5,12 +5,10 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import NMF
 from sklearn.metrics.pairwise import cosine_similarity
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
-from functools import lru_cache  # lru_cache 사용
 
 app_nmf = FastAPI()
-background_tasks = BackgroundTasks()
 
 # MySQL 연결 설정 (애플리케이션 시작 시에 연결을 열고 종료 시에 닫음)
 db = pymysql.connect(
@@ -22,153 +20,82 @@ db = pymysql.connect(
     charset='utf8'
 )
 
-# MySQL 데이터베이스에서 과거, 현재, 미래 데이터를 로드
+# MySQL 데이터베이스에서 과거 및 현재 데이터를 로드
 past_sql = "SELECT * FROM musical_past"
 present_sql = "SELECT * FROM musical_present"
-future_sql = "SELECT * FROM musical_future"
 
 # 데이터 로드
 past_data = pd.read_sql(past_sql, db)
 present_data = pd.read_sql(present_sql, db)
-future_data = pd.read_sql(future_sql, db)
 
 # MySQL 연결 닫기 (애플리케이션이 종료될 때)
 db.close()
 
-# NMF 모델 초기화
-nmf = NMF(n_components=10, init='random', random_state=42)
+# 추가: 'synopsis_numpy_scale' 열의 데이터 타입 확인과 예시 값 출력
+print("Data type of 'synopsis_numpy_scale' in past_data:", past_data['synopsis_numpy_scale'].dtype)
+print("First few values of 'synopsis_numpy_scale' in past_data:", past_data['synopsis_numpy_scale'].head().tolist())
 
-# 과거 데이터 스케일링
-past_data['synopsis_numpy_scale'] = past_data['synopsis_numpy_scale'].apply(lambda x: np.array(json.loads(x)))
-scaler_past = StandardScaler()
-past_data_scaled = scaler_past.fit_transform(np.vstack(past_data['synopsis_numpy_scale']))
-past_data_scaled = past_data_scaled - np.min(past_data_scaled) + 1e-10
+# 추가: 'synopsis_numpy_scale' 열의 데이터 타입 확인과 예시 값 출력
+print("Data type of 'synopsis_numpy_scale' in present_data:", present_data['synopsis_numpy_scale'].dtype)
+print("First few values of 'synopsis_numpy_scale' in present_data:", present_data['synopsis_numpy_scale'].head().tolist())
 
-# 특성 행렬 생성
-V_past = np.vstack(past_data_scaled)
-
-# NMF 모델 훈련
-W_past = nmf.fit_transform(V_past)
-
-
-# lru_cache를 사용하여 결과를 캐싱하는 함수
-@lru_cache()
-def recommend_impl(musical_id: int):
+# 뮤지컬 추천을 위한 musical_id에 기반한 엔드포인트 정의
+@app_nmf.get("/{musical_id}")
+def recommend(musical_id: int):
     try:
         # 선택한 작품의 인덱스 찾기
-        selected_work_index_past = past_data[past_data['musical_id'] == musical_id].index
+        selected_work_index_past = past_data[past_data['musical_id'] == musical_id].index[0]
 
-        # 로그 추가
-        print(f"Debug: musical_id={musical_id}, selected_work_index_past={selected_work_index_past}")
+        # 과거 데이터 파싱 및 스케일링
+        past_data['synopsis_numpy_scale'] = past_data['synopsis_numpy_scale'].apply(lambda x: np.array(json.loads(x.decode('utf-8')) if isinstance(x, bytes) else x))
+        scaler_past = StandardScaler()
+        past_data_scaled = scaler_past.fit_transform(np.vstack(past_data['synopsis_numpy_scale']))
+        past_data_scaled = past_data_scaled - np.min(past_data_scaled) + 1e-10
 
         # 현재 작품 선택
-        present_data['synopsis_numpy_scale'] = present_data['synopsis_numpy_scale'].apply(
-            lambda x: np.array(json.loads(x)))
+        present_data['synopsis_numpy_scale'] = present_data['synopsis_numpy_scale'].apply(lambda x: np.array(json.loads(x.decode('utf-8')) if isinstance(x, bytes) else x))
         scaler_present = StandardScaler()
         present_data_scaled = scaler_present.fit_transform(np.vstack(present_data['synopsis_numpy_scale']))
         present_data_scaled = present_data_scaled - np.min(present_data_scaled) + 1e-10
 
+        # 'list' 객체에 'tolist' 속성이 없는 경우를 고려하여 tolist 호출
+        present_data['synopsis_numpy_scale'] = present_data['synopsis_numpy_scale'].apply(lambda x: x.tolist() if hasattr(x, 'tolist') else x if x is not None else [])
+
+        # NMF 모델 초기화
+        nmf = NMF(n_components=10, init='random', random_state=42, max_iter=500)
+
+        # 특성 행렬 생성
+        V = np.vstack(past_data_scaled)
+
+        # NMF 모델 훈련
+        W = nmf.fit_transform(V)
+
         # 현재 상영중인 데이터에 대한 특성 행렬 생성
         V_present = np.vstack(present_data_scaled)
-
-        # 미래 데이터 선택
-        future_data['synopsis_numpy_scale'] = future_data['synopsis_numpy_scale'].apply(
-            lambda x: np.array(json.loads(x)))
-        scaler_future = StandardScaler()
-        future_data_scaled = scaler_future.fit_transform(np.vstack(future_data['synopsis_numpy_scale']))
-        future_data_scaled = future_data_scaled - np.min(future_data_scaled) + 1e-10
-
-        # 미래 데이터에 대한 특성 행렬 생성
-        V_future = np.vstack(future_data_scaled)
 
         # NMF 모델을 사용하여 현재 상영중인 데이터의 특성 행렬 분해
         W_present = nmf.transform(V_present)
 
-        # NMF 모델을 사용하여 미래 데이터의 특성 행렬 분해
-        W_future = nmf.transform(V_future)
+        # 선택한 작품과 다른 작품 간의 코사인 유사도 계산
+        selected_work = W[selected_work_index_past].reshape(1, -1)
+        similarities = cosine_similarity(W_present, selected_work)
 
-        # 선택한 작품과 다른 작품 간의 코사인 유사도 계산 (현재 데이터)
-        selected_work_present = W_present[selected_work_index_past].reshape(1, -1)
-        similarities_present = cosine_similarity(W_present, selected_work_present)
+        # 유사도가 높은 순서대로 정렬하여 유사한 작품 인덱스를 찾기
+        similar_work_indices = similarities.argsort(axis=0)[::-1].flatten()
+        top_n = min(5, len(similar_work_indices))
 
-        # 선택한 작품과 다른 작품 간의 코사인 유사도 계산 (과거 데이터)
-        selected_work_past = W_past[selected_work_index_past].reshape(1, -1)
-        similarities_past = cosine_similarity(W_past, selected_work_past)
-
-        # 선택한 작품과 다른 작품 간의 코사인 유사도 계산 (미래 데이터)
-        selected_work_future = W_future[selected_work_index_past].reshape(1, -1)
-        similarities_future = cosine_similarity(W_future, selected_work_future)
-
-        # 유사도가 높은 순서대로 정렬하여 유사한 작품 인덱스를 찾기 (현재 데이터)
-        similar_work_indices_present = similarities_present.argsort(axis=0)[::-1].flatten()
-        top_n_present = min(5, len(similar_work_indices_present))
-
-        # 유사도가 높은 순서대로 정렬하여 유사한 작품 인덱스를 찾기 (과거 데이터)
-        similar_work_indices_past = similarities_past.argsort(axis=0)[::-1].flatten()
-        top_n_past = min(5, len(similar_work_indices_past))
-
-        # 유사도가 높은 순서대로 정렬하여 유사한 작품 인덱스를 찾기 (미래 데이터)
-        similar_work_indices_future = similarities_future.argsort(axis=0)[::-1].flatten()
-        top_n_future = min(5, len(similar_work_indices_future))
-
-        # 상위 N개의 유사한 작품에 대한 정보를 추출하고 결과 리스트에 추가 (현재 데이터)
-        present_result = []
-        for i in range(top_n_present):
-            index = similar_work_indices_present[i]
-            similarity = float(similarities_present[index])
+        # 상위 N개의 유사한 작품에 대한 정보를 추출하고 결과 리스트에 추가
+        result = []
+        for i in range(top_n):
+            index = similar_work_indices[i]
+            similarity = float(similarities[index])
+            # 작품 정보 추출
             title = present_data.loc[index, 'title']
             musical_id = int(present_data.loc[index, 'musical_id'])
-            present_result.append({"title": title, "musical_id": musical_id, "similarity": similarity})
+            # 결과 리스트에 작품 정보를 추가
+            result.append({"title": title, "musical_id": musical_id, "similarity": similarity})
 
-        # 상위 N개의 유사한 작품에 대한 정보를 추출하고 결과 리스트에 추가 (과거 데이터)
-        past_result = []
-        for i in range(top_n_past):
-            index = similar_work_indices_past[i]
-            similarity = float(similarities_past[index])
-            title = past_data.loc[index, 'title']
-            musical_id = int(past_data.loc[index, 'musical_id'])
-            past_result.append({"title": title, "musical_id": musical_id, "similarity": similarity})
-
-        # 상위 N개의 유사한 작품에 대한 정보를 추출하고 결과 리스트에 추가 (미래 데이터)
-        future_result = []
-        for i in range(top_n_future):
-            index = similar_work_indices_future[i]
-            similarity = float(similarities_future[index])
-            title = future_data.loc[index, 'title']
-            musical_id = int(future_data.loc[index, 'musical_id'])
-            future_result.append({"title": title, "musical_id": musical_id, "similarity": similarity})
-
-        # 현재, 과거, 미래 결과를 반환
-        return {"present": present_result, "past": past_result, "future": future_result}
-
+        return result
     except Exception as e:
         # 예외가 발생한 경우, 에러 응답을 반환
-        return {"error": f"An error occurred: {str(e)}"}
-
-# FastAPI 엔드포인트에서 캐시를 사용하도록 수정
-@app_nmf.get("/recommend/{musical_id}")
-async def recommend(musical_id: int):
-    try:
-        # 결과 반환
-        result = recommend_impl(musical_id)
-
-        # JSON으로 직렬화 가능한 형태로 결과 가공
-        present_result = result.get("present", [])
-        past_result = result.get("past", [])
-        future_result = result.get("future", [])
-
-        present_result_json = json.dumps(present_result)
-        past_result_json = json.dumps(past_result)
-        future_result_json = json.dumps(future_result)
-
-        # 최종 결과 반환
-        return {
-            "result": {
-                "present": json.loads(present_result_json),
-                "past": json.loads(past_result_json),
-                "future": json.loads(future_result_json),
-            }
-        }
-
-    except Exception as e:
-        return {"result": {"error": f"An error occurred: {str(e)}"}}
+        return HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
--- Fast_API/app_nmf_future.py
+++ Fast_API/app_nmf_future.py
@@ -5,12 +5,10 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import NMF
 from sklearn.metrics.pairwise import cosine_similarity
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
-from functools import lru_cache  # lru_cache 사용
 
-app_nmf = FastAPI()
-background_tasks = BackgroundTasks()
+app_nmf_future = FastAPI()
 
 # MySQL 연결 설정 (애플리케이션 시작 시에 연결을 열고 종료 시에 닫음)
 db = pymysql.connect(
@@ -22,144 +20,83 @@ db = pymysql.connect(
     charset='utf8'
 )
 
-# MySQL 데이터베이스에서 과거, 현재, 미래 데이터를 로드
+# MySQL 데이터베이스에서 과거 및 미래 데이터를 로드
 past_sql = "SELECT * FROM musical_past"
-present_sql = "SELECT * FROM musical_present"
 future_sql = "SELECT * FROM musical_future"
 
 # 데이터 로드
 past_data = pd.read_sql(past_sql, db)
-present_data = pd.read_sql(present_sql, db)
 future_data = pd.read_sql(future_sql, db)
 
 # MySQL 연결 닫기 (애플리케이션이 종료될 때)
 db.close()
 
-# NMF 모델 초기화
-nmf = NMF(n_components=10, init='random', random_state=42)
+# 추가: 'synopsis_numpy_scale' 열의 데이터 타입 확인과 예시 값 출력
+print("Data type of 'synopsis_numpy_scale' in past_data:", past_data['synopsis_numpy_scale'].dtype)
+print("First few values of 'synopsis_numpy_scale' in past_data:", past_data['synopsis_numpy_scale'].head().tolist())
 
-# 과거 데이터 스케일링
-past_data['synopsis_numpy_scale'] = past_data['synopsis_numpy_scale'].apply(lambda x: np.array(json.loads(x)))
-scaler_past = StandardScaler()
-past_data_scaled = scaler_past.fit_transform(np.vstack(past_data['synopsis_numpy_scale']))
-past_data_scaled = past_data_scaled - np.min(past_data_scaled) + 1e-10
+# 추가: 'synopsis_numpy_scale' 열의 데이터 타입 확인과 예시 값 출력
+print("Data type of 'synopsis_numpy_scale' in future_data:", future_data['synopsis_numpy_scale'].dtype)
+print("First few values of 'synopsis_numpy_scale' in future_data:", future_data['synopsis_numpy_scale'].head().tolist())
 
-# 특성 행렬 생성
-V_past = np.vstack(past_data_scaled)
-
-# NMF 모델 훈련
-W_past = nmf.fit_transform(V_past)
-
-
-# lru_cache를 사용하여 결과를 캐싱하는 함수
-@lru_cache()
-def recommend_impl(musical_id: int):
+# 뮤지컬 추천을 위한 musical_id에 기반한 엔드포인트 정의
+@app_nmf_future.get("/{musical_id}")
+def recommend_future(musical_id: int):
     try:
         # 선택한 작품의 인덱스 찾기
-        selected_work_index_past = past_data[past_data['musical_id'] == musical_id].index
+        selected_work_index_past = past_data[past_data['musical_id'] == musical_id].index[0]
 
-        # 현재 작품 선택
-        present_data['synopsis_numpy_scale'] = present_data['synopsis_numpy_scale'].apply(
-            lambda x: np.array(json.loads(x.decode('utf-8'))))
-        scaler_present = StandardScaler()
-        present_data_scaled = scaler_present.fit_transform(np.vstack(present_data['synopsis_numpy_scale']))
-        present_data_scaled = present_data_scaled - np.min(present_data_scaled) + 1e-10
+        # 과거 데이터 파싱 및 스케일링
+        past_data['synopsis_numpy_scale'] = past_data['synopsis_numpy_scale'].apply(lambda x: np.array(json.loads(x.decode('utf-8')) if isinstance(x, bytes) else x)) # bytes 형태인 경우에만 decode를 적용하도록 수정
+        scaler_past = StandardScaler()
+        past_data_scaled = scaler_past.fit_transform(np.vstack(past_data['synopsis_numpy_scale']))
+        past_data_scaled = past_data_scaled - np.min(past_data_scaled) + 1e-10
 
-        # 현재 상영중인 데이터에 대한 특성 행렬 생성
-        V_present = np.vstack(present_data_scaled)
 
-        # 미래 데이터 선택
-        future_data['synopsis_numpy_scale'] = future_data['synopsis_numpy_scale'].apply(
-            lambda x: np.array(json.loads(x.decode('utf-8'))))
+        # 미래 작품 선택
+        future_data['synopsis_numpy_scale'] = future_data['synopsis_numpy_scale'].apply(lambda x: np.array(json.loads(x.decode('utf-8')) if isinstance(x, bytes) else x))
         scaler_future = StandardScaler()
         future_data_scaled = scaler_future.fit_transform(np.vstack(future_data['synopsis_numpy_scale']))
         future_data_scaled = future_data_scaled - np.min(future_data_scaled) + 1e-10
 
-        # 미래 데이터에 대한 특성 행렬 생성
+        # 'list' 객체에 'tolist' 속성이 없는 경우를 고려하여 tolist 호출
+        future_data['synopsis_numpy_scale'] = future_data['synopsis_numpy_scale'].apply(lambda x: x.tolist() if hasattr(x, 'tolist') else x if x is not None else [])
+
+        # NMF 모델 초기화
+        nmf = NMF(n_components=10, init='random', random_state=42, max_iter=500)
+
+        # 특성 행렬 생성
+        V = np.vstack(past_data_scaled)
+
+        # NMF 모델 훈련
+        W = nmf.fit_transform(V)
+
+        # 미래 상영중인 데이터에 대한 특성 행렬 생성
         V_future = np.vstack(future_data_scaled)
 
-        # NMF 모델을 사용하여 현재 상영중인 데이터의 특성 행렬 분해
-        W_present = nmf.transform(V_present)
-
-        # NMF 모델을 사용하여 미래 데이터의 특성 행렬 분해
+        # NMF 모델을 사용하여 미래 상영중인 데이터의 특성 행렬 분해
         W_future = nmf.transform(V_future)
 
-        # 선택한 작품과 다른 작품 간의 코사인 유사도 계산 (현재 데이터)
-        selected_work_present = W_present[selected_work_index_past].reshape(1, -1)
-        similarities_present = cosine_similarity(W_present, selected_work_present)
+        # 선택한 작품과 다른 작품 간의 코사인 유사도 계산
+        selected_work = W[selected_work_index_past].reshape(1, -1)
+        similarities = cosine_similarity(W_future, selected_work)
 
-        # 선택한 작품과 다른 작품 간의 코사인 유사도 계산 (과거 데이터)
-        selected_work_past = W_past[selected_work_index_past].reshape(1, -1)
-        similarities_past = cosine_similarity(W_past, selected_work_past)
+        # 유사도가 높은 순서대로 정렬하여 유사한 작품 인덱스를 찾기
+        similar_work_indices = similarities.argsort(axis=0)[::-1].flatten()
+        top_n = min(5, len(similar_work_indices))
 
-        # 선택한 작품과 다른 작품 간의 코사인 유사도 계산 (미래 데이터)
-        selected_work_future = W_future[selected_work_index_past].reshape(1, -1)
-        similarities_future = cosine_similarity(W_future, selected_work_future)
-
-        # 유사도가 높은 순서대로 정렬하여 유사한 작품 인덱스를 찾기 (현재 데이터)
-        similar_work_indices_present = similarities_present.argsort(axis=0)[::-1].flatten()
-        top_n_present = min(5, len(similar_work_indices_present))
-
-        # 유사도가 높은 순서대로 정렬하여 유사한 작품 인덱스를 찾기 (과거 데이터)
-        similar_work_indices_past = similarities_past.argsort(axis=0)[::-1].flatten()
-        top_n_past = min(5, len(similar_work_indices_past))
-
-        # 유사도가 높은 순서대로 정렬하여 유사한 작품 인덱스를 찾기 (미래 데이터)
-        similar_work_indices_future = similarities_future.argsort(axis=0)[::-1].flatten()
-        top_n_future = min(5, len(similar_work_indices_future))
-
-        # 현재 데이터에 대한 추천 결과
-        present_result = []
-        for i in range(top_n_present):
-            index = similar_work_indices_present[i]
-            similarity = float(similarities_present[index])
-            title = present_data.loc[index, 'title']
-            musical_id = int(present_data.loc[index, 'musical_id'])
-            present_result.append({"title": title, "musical_id": musical_id, "similarity": similarity})
-
-        # 과거 데이터에 대한 추천 결과
-        past_result = []
-        for i in range(top_n_past):
-            index = similar_work_indices_past[i]
-            similarity = float(similarities_past[index])
-            title = past_data.loc[index, 'title']
-            musical_id = int(past_data.loc[index, 'musical_id'])
-            past_result.append({"title": title, "musical_id": musical_id, "similarity": similarity})
-
-        # 미래 데이터에 대한 추천 결과
-        future_result = []
-        for i in range(top_n_future):
-            index = similar_work_indices_future[i]
-            similarity = float(similarities_future[index])
+        # 상위 N개의 유사한 작품에 대한 정보를 추출하고 결과 리스트에 추가
+        result = []
+        for i in range(top_n):
+            index = similar_work_indices[i]
+            similarity = float(similarities[index])
+            # 작품 정보 추출
             title = future_data.loc[index, 'title']
             musical_id = int(future_data.loc[index, 'musical_id'])
-            future_result.append({"title": title, "musical_id": musical_id, "similarity": similarity})
+            # 결과 리스트에 작품 정보를 추가
+            result.append({"title": title, "musical_id": musical_id, "similarity": similarity})
 
-        return {"present": present_result, "past": past_result, "future": future_result}
-
+        return result
     except Exception as e:
         # 예외가 발생한 경우, 에러 응답을 반환
-        return JSONResponse(content={"error": f"An error occurred: {str(e)}"}, status_code=500)
-
-
-# FastAPI 엔드포인트에서 캐시를 사용하도록 수정
-@app_nmf.get("/recommend/{musical_id}")
-async def recommend(musical_id: int, background_tasks: BackgroundTasks):
-    try:
-        # 캐시된 결과 확인
-        cached_result = recommend_impl(musical_id)
-
-        if cached_result is None:
-            # 결과가 캐시되어 있지 않으면 계산하고 캐시에 저장
-            background_tasks.add_task(recommend_impl, musical_id)
-            return {"message": "Task added to background. Please try again."}
-        else:
-            # 캐시된 결과 반환
-            return {"result": cached_result}
-
-    except Exception as e:
-        return {"error": f"An error occurred: {str(e)}"}
-
-
-# 실행방법 : uvicorn app_nmf:app_nmf --reload --host 0.0.0.0 --port 8080
-# 주소검색 : http://localhost:8080/recommend/{musical_id}
+        return HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
